5a9c09bcde3ed6ae4eba81d45441b6127799be8b
mexas <mexas@bris.ac.uk>
2016-08-12 11:25:43 +0000

    added a makefile for ifort

--- errtest.f90
+++ errtest.f90
@@ -7,14 +7,52 @@ program errtest
 use errfun
 implicit none
 
-complex( kind=rkind ) :: z
-real( kind=rkind ) :: err = eps0
+! Use np points along x and y.
+integer, parameter :: np = 1000
+real( kind=rkind ), parameter :: xmin = -10.0_rkind,                   &
+  xmax = -xmin, ymin= xmin, ymax = xmax, delta = (xmax-xmin)/(np-1)
+complex( kind=rkind ) :: z(np,np), res(np,np)
+real( kind=rkind ) :: x(np), y(np), err = 0.06447_rkind * eps0,  &
+  re(np,np), im(np,np)
+real :: time2, time1
 
-z = cmplx( -0.0_rkind, 1.0_rkind, kind=rkind )
-write (*,*) fad( z, err )
+integer :: i,j,funit
 
-z = cmplx( 6.3_rkind, 1.0e-20_rkind, kind=rkind )
-write (*,*) "z, w(z):", z, fad( z, err )
-write (*,*) "z, erf_cmplx(z):", z, erf_cmplx( z, err )
+
+! Generate the coordinate arrays
+do concurrent( i=1:np )
+  x(i) = xmin + delta * (i-1)
+  y(i) = ymin + delta * (i-1)
+end do
+
+!write (*,*) "delta:", delta
+!write (*,*) "xmax, ymax:", x(np), y(np)
+
+! Generate the array of z
+do concurrent (i=1:np, j=1:np)
+  z(i,j) = cmplx( x(i), y(j), kind=rkind ) 
+end do
+
+! Time the calculation
+call cpu_time( time1 )
+res = fad( z, err )
+call cpu_time( time2 )
+write (*,*) "CPU time, s:", time2-time1
+
+writeout: if (.false.) then
+  ! Write the data out
+  re = real( res )
+  im = aimag( res )
+  
+  open(newunit=funit, file="out-fortran", form="formatted",              &
+       status="replace")
+  do j=1,np
+  do i=1,np
+    write (funit, "(4(es25.16,tr1))") x(i), y(j), re(i,j), im(i,j)
+  end do
+  end do
+  close(funit)
+
+end if writeout
 
 end program errtest
